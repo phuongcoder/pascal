d445e04ff4790a5367d976e0b966b925a07b10e0
phuongcoder <97374006+phuongcoder@users.noreply.github.com>
2022-02-21 20:57:17 +0700

    tim_so_lon_nhat_cua_day_so_nguyen

---
+++ tim_so_lon_nhat_cua_day_so_nguyen.pas
@@ -0,0 +1,26 @@
+program TimMax;
+uses crt;
+const Nmax = 250;
+type ArrInt = array[1..Nmax] of integer;
+var N, i, Max, csmax:integer;
+    A:ArrInt;
+begin
+    clrscr;
+    write('Nhap so luong phan tu cua day so, N = ');
+    readln(N);
+    for i:= 1 to N do
+        begin
+            write('Phan tu thu ',i,' = ');
+            readln(A[i]);
+        end;
+    Max:= A[1]; csmax:=1;
+    for i:= 2 to N do
+        if A[i] > Max then
+            begin
+                Max:= A[i];
+                csmax:= i;
+            end;
+        writeln('Gia tri cua phan tu Max: ',Max);
+        writeln('Chi so cua phan tu Max: ',csmax);
+    readln
+end.
