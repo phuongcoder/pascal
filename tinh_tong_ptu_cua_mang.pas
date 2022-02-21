72a5892986c1fcc6088bc62f9f67a77be0501674
phuongcoder <97374006+phuongcoder@users.noreply.github.com>
2022-02-21 22:09:53 +0700

    Tinh_tong_phan_tu_cua_mang
    Tạo mảng A gồm n (n<=100) số nguyên, mỗi số có giá trị tuyệt đối không vượt quá 300. Tính tổng các phần tử của mảng là bội số của một số nguyên dương k cho trước.

---
+++ tinh_tong_ptu_cua_mang.pas
@@ -0,0 +1,22 @@
+program Suml;
+uses crt;
+const nmax=100;
+type MyArray = array[1..nmax] of integer;
+var A: MyArray;
+    s, n, i, k: integer;
+begin
+    clrscr;
+    randomize;
+    write('Nhap n = ');
+    readln(n); {Tao ngau nhien mang gom n so nguyen}
+        for i:= 1 to n do A[i]:= random(300) - random(300);
+        for i:= 1 to n do write(A[i]:5); {In ra mang vua tao}
+    writeln;
+    write('Nhap k = ');
+    readln(k);
+        s:=0;
+        for i:=1 to n do
+            if A[i] mod k = 0 then s:=s+A[i];
+        writeln('Tong can tinh la: ',s);
+    readln
+end.
